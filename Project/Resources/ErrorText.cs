b554fdf1960f9fea28e311f24d4dd2b9216d4465
enigma81 <emilmacesic@gmail.com>
2016-06-25 13:00:57 +0200

    Readme.txt

---
+++ Project/Resources/ErrorText.cs
@@ -0,0 +1,9 @@
+﻿namespace Project.Resources
+{
+    class ErrorText
+    {
+        public const string ValidatorNameError = "You need to insert valid string value.";
+        public const string ValidatorGpaError = "You need to insert numerical value.";
+        public const string ValidatorOperationError = "Operation non-existing, please use appropriate operation.";
+    }
+}
